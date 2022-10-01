400ffbfd0ad0da5b5b65d68e76c98fb3077dce65
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-01 17:09:14 +0900

    ⚙️ :: Configuration Scanner 추가

---
+++ src/main/kotlin/com/project/gatihaeyo/global/config/ConfigurationPropertiesConfig.kt
@@ -0,0 +1,8 @@
+package com.project.gatihaeyo.global.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@ConfigurationPropertiesScan("com.project.gatihaeyo")
+@Configuration
+class ConfigurationPropertiesConfig
