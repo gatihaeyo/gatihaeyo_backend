218f9692f73e1f96448f50ac6ffa17ce952a0fb4
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-28 01:09:39 +0900

    add: OpenApi port 추가

---
+++ src/main/kotlin/com/project/gatihaeyo/internal/user/application/port/OpenApiPort.kt
@@ -0,0 +1,14 @@
+package com.project.gatihaeyo.internal.user.application.port
+
+import com.project.gatihaeyo.external.openapi.dto.InfoRecordDto
+
+interface OpenApiPort {
+
+    fun getPUBGId(name: String): String
+
+    fun getPUBGInfo(key: String): List<InfoRecordDto>
+
+    fun getLOLId(name: String): String
+
+    fun getLOLInfo(key: String): List<InfoRecordDto>
+}
