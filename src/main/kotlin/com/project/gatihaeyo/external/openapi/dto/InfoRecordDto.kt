5db640007fbf225e27c5d06e97feecc074188231
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-10-28 01:00:02 +0900

    add: 계정 정보 dto

---
+++ src/main/kotlin/com/project/gatihaeyo/external/openapi/dto/InfoRecordDto.kt
@@ -0,0 +1,9 @@
+package com.project.gatihaeyo.external.openapi.dto
+
+data class InfoRecordDto(
+    val type: String? = null,
+    val tier: String,
+    val rank: String,
+    val point: Int,
+    val winRatio: Double,
+)
