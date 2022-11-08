1f4c100cfe5b733e33b13db4cc48edbbc725ef5d
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-08 19:34:07 +0900

    add: AuditJpaConfig

---
+++ src/main/kotlin/com/project/gatihaeyo/global/config/AuditJpaConfig.kt
@@ -0,0 +1,9 @@
+package com.project.gatihaeyo.global.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class AuditJpaConfig {
+}
