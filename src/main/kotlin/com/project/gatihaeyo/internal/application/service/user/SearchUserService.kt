ee73faeca0932a4a669cc654c199b0a25aa63a3a
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-08 19:40:03 +0900

    add: 유저 닉네임 검색 기능

---
+++ src/main/kotlin/com/project/gatihaeyo/internal/application/service/user/SearchUserService.kt
@@ -0,0 +1,15 @@
+package com.project.gatihaeyo.internal.application.service.user
+
+import com.project.gatihaeyo.internal.application.port.user.QueryUserPort
+import com.project.gatihaeyo.internal.domain.model.auth.User
+import org.springframework.stereotype.Service
+
+@Service
+class SearchUserService(
+    private val queryUserPort: QueryUserPort
+) {
+
+    fun execute(keyword: String): List<User> {
+        return queryUserPort.queryUserListByNickname(keyword)
+    }
+}
