9371c956d2003df13b8b977b4f4195fd7d9944de
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-09 19:18:26 +0900

    docs: 주석 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,24 +18,46 @@ repositories {
 }
 
 dependencies {
+
+    // aws
     implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
     implementation("com.amazonaws:aws-java-sdk-ses:1.12.307")
+
+    // mapstruct
     implementation("org.mapstruct:mapstruct:1.5.3.Final")
     kapt("org.mapstruct:mapstruct-processor:1.5.3.Final")
-    implementation("io.jsonwebtoken:jjwt:0.9.1")
-    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.5")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis:2.7.5")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.5")
-    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-    implementation("mysql:mysql-connector-java:8.0.30")
+
+    // security
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("io.jsonwebtoken:jjwt:0.9.1")
+
+    // validation
+    implementation("org.springframework.boot:spring-boot-starter-validation:2.7.5")
+
+    // redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:2.7.5")
+
+    // jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.5")
+
+    // web
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // openfeign
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.5")
+
+    // db
+    runtimeOnly("mysql:mysql-connector-java")
+    implementation("mysql:mysql-connector-java:8.0.30")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+
+    // configuration
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.4")
-    runtimeOnly("mysql:mysql-connector-java")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
@@ -48,6 +70,7 @@ kapt {
 }
 
 allOpen {
+    annotation("com.project.gatihaeyo.global.annotation.BusinessService")
     annotation("javax.persistence.Entity")
     annotation("javax.persistence.MappedSuperclass")
     annotation("javax.persistence.Embeddable")
