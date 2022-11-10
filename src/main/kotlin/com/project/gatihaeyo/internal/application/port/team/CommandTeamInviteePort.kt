6ff1d92cd4b1df02f80eb7d8babe378fba9fa418
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-10 23:22:19 +0900

    add: delete query 추가

--- src/main/kotlin/com/project/gatihaeyo/internal/application/port/team/CommandTeamInviteePort.kt
+++ src/main/kotlin/com/project/gatihaeyo/internal/application/port/team/CommandTeamInviteePort.kt
@@ -1,8 +1,11 @@
 package com.project.gatihaeyo.internal.application.port.team
 
 import com.project.gatihaeyo.internal.domain.model.team.TeamInvitee
+import java.util.UUID
 
 interface CommandTeamInviteePort {
 
     fun saveTeamInvitee(teamInvitee: TeamInvitee): TeamInvitee
+
+    fun deleteTeamInviteeByUserIdAndTeamId(userId: UUID, teamId: UUID)
 }
--- src/main/kotlin/com/project/gatihaeyo/internal/application/port/team/CommandTeamMemberPort.kt
+++ src/main/kotlin/com/project/gatihaeyo/internal/application/port/team/CommandTeamMemberPort.kt
@@ -1,8 +1,11 @@
 package com.project.gatihaeyo.internal.application.port.team
 
 import com.project.gatihaeyo.internal.domain.model.team.TeamMember
+import java.util.UUID
 
 interface CommandTeamMemberPort {
 
     fun saveTeamMember(teamMember: TeamMember): TeamMember
+
+    fun deleteTeamMemberByUserIdAndTeamId(userId: UUID, teamId: UUID)
 }
