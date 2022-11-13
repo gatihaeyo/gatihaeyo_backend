6309985f081d013b071058ec58c84970cf1b7e58
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-13 15:47:21 +0900

    refactor: 파티 모집 리스트

--- src/main/kotlin/com/project/gatihaeyo/internal/application/service/team/ShowTeamListService.kt
+++ src/main/kotlin/com/project/gatihaeyo/internal/application/service/team/ShowTeamListService.kt
@@ -3,28 +3,26 @@ package com.project.gatihaeyo.internal.application.service.team
 import com.project.gatihaeyo.global.annotation.ReadOnlyBusinessService
 import com.project.gatihaeyo.internal.application.port.team.QueryTeamPort
 import com.project.gatihaeyo.internal.dto.request.team.ShowTeamListRequest
-import com.project.gatihaeyo.internal.dto.response.team.ShowTeamResponse
+import com.project.gatihaeyo.internal.dto.response.team.ShowTeamListResponse
 
 @ReadOnlyBusinessService
 class ShowTeamListService(
     private val queryTeamPort: QueryTeamPort
 ) {
 
-    fun execute(request: ShowTeamListRequest): ShowTeamResponse {
+    fun execute(request: ShowTeamListRequest): ShowTeamListResponse {
         val list = queryTeamPort.queryTeamList(request.size, request.last, request.order, request.category)
 
-        return ShowTeamResponse(
+        return ShowTeamListResponse(
             list.map {
-                ShowTeamResponse.ShowTeamElement(
+                ShowTeamListResponse.ShowTeamListElement(
                     id = it.id,
                     master = it.master,
                     category = it.category,
                     title = it.title,
-                    content = it.content,
                     updateAt = it.updateAt,
                     personnel = it.personnel,
-                    currentPersonnel = it.currentPersonnel,
-                    applicantPersonnel = it.applicantPersonnel
+                    currentPersonnel = it.currentPersonnel
                 )
             }
         )
