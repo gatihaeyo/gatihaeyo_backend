6c3f01c70de463d7251238579a17f2a1dac08da3
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-17 19:49:00 +0900

    add: BuildSrc 모듈

---
+++ buildSrc/src/main/kotlin/DependencyVersions.kt
@@ -0,0 +1,12 @@
+object DependencyVersions {
+    const val JWT_VERSION = "0.9.1"
+    const val MAPSTRUCT_VERSION = "1.5.2.Final"
+    const val AWS_VERSION = "2.2.6.RELEASE"
+    const val REDIS_VERSION = "2.7.2"
+    const val SERVLET_VERSION = "4.0.1"
+    const val AWS_SES_VERSION = "1.12.296"
+    const val SPRING_TRANSACTION_VERSION = "5.3.22"
+    const val QUERYDSL_VERSION = "5.0.0"
+    const val BYTE_BUDDY_VERSION = "1.12.18"
+    const val OPEN_FEIGN_VERSION = "3.1.5"
+}
