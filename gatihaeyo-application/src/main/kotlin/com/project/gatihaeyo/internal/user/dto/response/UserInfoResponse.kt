8f80819531baebc5ca371b9f6d3c1e8c029c862e
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-17 21:22:55 +0900

    chore: 애플리케이션 계층 Response

--- gatihaeyo-application/src/main/kotlin/com/project/gatihaeyo/internal/user/dto/response/UserInfoResponse.kt
+++ gatihaeyo-application/src/main/kotlin/com/project/gatihaeyo/internal/user/dto/response/UserInfoResponse.kt
@@ -1,6 +1,6 @@
-package com.project.gatihaeyo.internal.dto.response.user
+package com.project.gatihaeyo.internal.user.dto.response
 
-import com.project.gatihaeyo.internal.domain.model.Category
+import com.project.gatihaeyo.internal.Category
 import java.util.UUID
 
 data class UserInfoResponse(
---
+++ gatihaeyo-application/src/main/kotlin/com/project/gatihaeyo/internal/user/dto/response/UserResponse.kt
@@ -0,0 +1,10 @@
+package com.project.gatihaeyo.internal.user.dto.response
+
+import java.util.UUID
+
+data class UserResponse(
+    val id: UUID,
+    val nickname: String,
+    val email: String,
+    val profileImagePath: String
+ )
