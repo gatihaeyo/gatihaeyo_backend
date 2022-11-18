e260352f40cf71848b8174c0da278c19b223eb12
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-18 21:35:15 +0900

    setting: 아키텍쳐 변경

--- src/main/kotlin/com/project/gatihaeyo/internal/application/port/user/CommandUserPort.kt
+++
@@ -1,9 +0,0 @@
-package com.project.gatihaeyo.internal.application.port.user
-
-import com.project.gatihaeyo.internal.domain.model.user.User
-
-interface CommandUserPort {
-
-    fun save(model: User): User
-
-}
