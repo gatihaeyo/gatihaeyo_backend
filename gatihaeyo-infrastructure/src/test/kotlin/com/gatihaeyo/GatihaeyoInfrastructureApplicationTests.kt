d7f2659397420cef043069c3c2524450b0a31ade
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-18 21:30:31 +0900

    test: application test

---
+++ gatihaeyo-infrastructure/src/test/kotlin/com/gatihaeyo/GatihaeyoInfrastructureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gatihaeyo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatihaeyoInfrastructureApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
