07228241c68a53126a4ea45bafebe4614a40ea72
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-19 23:42:24 +0900

    add: 알림 API

---
+++ gatihaeyo-presentation/src/main/kotlin/com/project/gatihaeyo/notice/NoticeController.kt
@@ -0,0 +1,35 @@
+package com.project.gatihaeyo.notice
+
+import com.project.gatihaeyo.internal.notice.service.RemoveNoticeService
+import com.project.gatihaeyo.internal.notice.service.ShowNoticeListService
+import com.project.gatihaeyo.notice.dto.NoticeListResponse
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController
+@RequestMapping("/notices")
+class NoticeController(
+    private val showNoticeListService: ShowNoticeListService,
+    private val removeNoticeService: RemoveNoticeService
+) {
+
+    @GetMapping
+    fun noticeList() : NoticeListResponse {
+        return NoticeListResponse(
+            showNoticeListService.execute()
+        )
+    }
+
+    @DeleteMapping("/{notice-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun deleteNotice(@PathVariable("notice-id") noticeId: UUID) {
+        removeNoticeService.execute(noticeId)
+    }
+
+}
