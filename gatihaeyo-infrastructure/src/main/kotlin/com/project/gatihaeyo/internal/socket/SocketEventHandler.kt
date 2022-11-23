cf45d47bc7cd03942913ea9c29b16935374160a4
Jo Kyung Hyeon <khcho0125@icloud.com>
2022-11-24 08:35:44 +0900

    refactor: 소켓 수정

--- gatihaeyo-infrastructure/src/main/kotlin/com/project/gatihaeyo/internal/socket/SocketEventHandler.kt
+++ gatihaeyo-infrastructure/src/main/kotlin/com/project/gatihaeyo/internal/socket/SocketEventHandler.kt
@@ -1,19 +1,23 @@
 package com.project.gatihaeyo.internal.socket
 
+import com.corundumstudio.socketio.SocketIOClient
 import com.corundumstudio.socketio.SocketIONamespace
 import com.corundumstudio.socketio.SocketIOServer
+import com.corundumstudio.socketio.listener.ConnectListener
 import com.corundumstudio.socketio.listener.DataListener
+import com.corundumstudio.socketio.listener.DisconnectListener
 import com.project.gatihaeyo.global.security.token.JwtParser
 import com.project.gatihaeyo.internal.message.dto.response.ShowMessageResponse
 import com.project.gatihaeyo.internal.message.implement.MessageFacade
 import com.project.gatihaeyo.internal.message.model.Message
 import com.project.gatihaeyo.internal.socket.dto.ChatMessageDto
-import com.project.gatihaeyo.internal.socket.dto.JoinRoomDto
 import com.project.gatihaeyo.internal.socket.exception.MessageNullException
 import com.project.gatihaeyo.internal.team.exception.TeamMemberNotFoundException
 import com.project.gatihaeyo.internal.team.implement.TeamMemberFacade
 import org.springframework.stereotype.Component
+import java.time.LocalDateTime
 import java.util.UUID
+import java.util.concurrent.ConcurrentHashMap
 
 @Component
 class SocketEventHandler(
@@ -23,21 +27,43 @@ class SocketEventHandler(
     private val messageFacade: MessageFacade
 ) {
 
+    init {
+        socketIOServer.apply {
+            addConnectListener(connectListener)
+            addDisconnectListener(disConnectListener)
+        }.start()
+    }
+
+    private val connectMap = ConcurrentHashMap<UUID, SocketIOClient>()
+
+    private val connectListener = (ConnectListener { client ->
+        val token = client.handshakeData.getSingleUrlParam("token")
+
+        val currentUserId = UUID.fromString(jwtParser.getAuthentication(token).name)
+
+        connectMap[currentUserId] = client
+        client.set("id", currentUserId)
+    })
+
+    private val disConnectListener = (DisconnectListener { client ->
+        val id = client.get<UUID>("id")
+        connectMap.remove(id)
+        client.disconnect()
+    })
+
     private val socketIONamespace: SocketIONamespace = socketIOServer.addNamespace("/chat").apply {
         addEventListener("send", ChatMessageDto::class.java, onMessage())
-        addEventListener("join", JoinRoomDto::class.java, onJoin())
+        addEventListener("join", UUID::class.java, onJoin())
         addEventListener("leave", UUID::class.java, onLeave())
     }
 
     private fun onMessage() : DataListener<ChatMessageDto> {
         return (DataListener { client, data, _ ->
-            if (data.token == null || data.message == null || data.roomId == null) {
+            if (data.message == null || data.roomId == null) {
                 throw MessageNullException.EXCEPTION
             }
 
-            val userId = UUID.fromString(
-                jwtParser.getAuthentication(data.token).name
-            )
+            val userId: UUID = client.get("id")
 
             val message = messageFacade.saveMessage(
                 Message(
@@ -55,30 +81,34 @@ class SocketEventHandler(
             )
 
             socketIONamespace.getRoomOperations(data.roomId.toString()).sendEvent("message", client, response)
+
+            println("SOCKET :: [{${LocalDateTime.now()}}] :: [$userId] - MESSAGE ${data.message}")
         })
     }
 
-    private fun onJoin() : DataListener<JoinRoomDto> {
+    private fun onJoin() : DataListener<UUID> {
         return (DataListener { client, data, _ ->
-            if (data.token == null || data.roomId == null) {
+            if (data == null) {
                 throw MessageNullException.EXCEPTION
             }
 
-            val userId = UUID.fromString(
-                jwtParser.getAuthentication(data.token).name
-            )
+            val userId: UUID = client.get("id")
 
-            if (!teamMemberFacade.existsTeamMemberByUserIdAndTeamId(userId, data.roomId)) {
+            if (!teamMemberFacade.existsTeamMemberByUserIdAndTeamId(userId, data)) {
                 throw TeamMemberNotFoundException.EXCEPTION
             }
 
-            client.joinRoom(data.roomId.toString())
+            client.joinRoom(data.toString())
+
+            println("SOCKET :: [{${LocalDateTime.now()}}] :: [$userId] - JOIN ROOM $data")
         })
     }
 
     private fun onLeave() : DataListener<UUID> {
         return (DataListener { client, data, _ ->
             client.leaveRoom(data.toString())
+
+            println("SOCKET :: [{${LocalDateTime.now()}}] :: [${client.get<String>("id")}] - LEAVE ROOM $data")
         })
     }
 
